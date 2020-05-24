6f4dca372933e1af1e2cdebea764d5bbafa01165
Austin Jones <ajone239@vols.utk.edu>
2020-05-24 13:12:17 -0400

    first commit

---
+++ Sudran.py
@@ -0,0 +1,180 @@
+import numpy as np
+from numpy import random
+import time
+
+# Setting up un-solved puzzle
+# '''
+puz = np.array(
+    [[0, 7, 0, 0, 5, 0, 0, 0, 0],
+     [0, 5, 1, 6, 0, 9, 2, 0, 0],
+     [4, 0, 6, 2, 0, 7, 0, 0, 1],
+     [0, 8, 7, 0, 0, 0, 1, 0, 0],
+     [3, 0, 0, 8, 0, 4, 0, 0, 2],
+     [0, 0, 4, 0, 0, 0, 5, 9, 0],
+     [1, 0, 0, 7, 0, 5, 8, 0, 9],
+     [0, 0, 8, 3, 0, 2, 4, 7, 0],
+     [0, 0, 0, 0, 4, 0, 0, 1, 0]])
+'''
+puz = np.array(
+    [[0, 2, 0, 4, 5, 6, 7, 8, 9],
+     [4, 5, 7, 0, 8, 0, 2, 3, 6],
+     [6, 8, 9, 2, 3, 7, 0, 4, 0],
+     [0, 0, 5, 3, 6, 2, 9, 7, 4],
+     [2, 7, 4, 0, 9, 0, 6, 5, 3],
+     [3, 9, 6, 5, 7, 4, 8, 0, 0],
+     [0, 4, 0, 6, 1, 8, 3, 9, 7],
+     [7, 6, 1, 0, 4, 0, 5, 2, 8],
+     [9, 3, 8, 7, 2, 5, 0, 6, 0]])
+'''
+
+t0 = time.time()
+
+startnum = puz > 0
+print(startnum)
+print(puz)
+d1 = 0
+r1 = 0
+correct = 1
+found = 1
+while found > 0:
+    found = 0
+    while d1 < 9:
+        if startnum[d1, r1] == False:
+            print('Index: ', d1, r1)
+            orig = puz[d1, r1]
+            row = puz[d1, 0:9]
+            col = puz[0:9, r1]
+            if d1 < 3 and r1 < 3:
+                block = puz[0:3, 0:3]
+            elif d1 < 3 and 2 < r1 < 6:
+                block = puz[0:3, 3:6]
+            elif d1 < 3 and r1 > 5:
+                block = puz[0:3, 6:9]
+            elif 2 < d1 < 6 and r1 < 3:
+                block = puz[3:6, 0:3]
+            elif 2 < d1 < 6 and 2 < r1 < 6:
+                block = puz[3:6, 3:6]
+            elif 2 < d1 < 6 and r1 > 5:
+                block = puz[3:6, 6:9]
+            elif 5 < d1 and r1 < 3:
+                block = puz[6:9, 0:3]
+            elif 5 < d1 and 2 < r1 < 6:
+                block = puz[6:9, 3:6]
+            elif 5 < d1 and 5 < r1:
+                block = puz[6:9, 6:9]
+            else:
+                print("Index not in bounds so cant find block")
+
+            print('Column: \n', col, ' \nRow: \n', row, ' \nBlock: \n', block)
+            print('Original:', orig)
+
+            guess = 1
+            poss = [0]
+            howman = 0
+            while guess < 10:
+                if guess not in row and guess not in col and guess not in block and orig != guess:
+                    poss.append(guess)
+                    howman += 1
+                guess += 1
+            if howman == 1:
+                found += 1
+                puz[d1, r1] = poss[1]
+        print(puz)
+        if r1 < 8:
+            r1 += 1
+        else:
+            d1 += 1
+            r1 = 0
+
+afterfirst = puz
+print('Puzzle after first solve: \n', puz)
+
+startnum = puz > 0
+# Start algorithm
+zer = 0
+down = 0
+right = 0
+correct = 1
+block = np.array(
+    [[0, 0, 0],
+     [0, 0, 0],
+     [0, 0, 0]])
+# print(block)
+
+
+while zer in puz:
+    if startnum[down, right] == False:
+        print('Index: ', down, right)
+        orig = puz[down, right]
+        row = puz[down, 0:9]
+        col = puz[0:9, right]
+        if down < 3 and right < 3:
+            block = puz[0:3, 0:3]
+        elif down < 3 and 2 < right < 6:
+            block = puz[0:3, 3:6]
+        elif down < 3 and right > 5:
+            block = puz[0:3, 6:9]
+        elif 2 < down < 6 and right < 3:
+            block = puz[3:6, 0:3]
+        elif 2 < down < 6 and 2 < right < 6:
+            block = puz[3:6, 3:6]
+        elif 2 < down < 6 and right > 5:
+            block = puz[3:6, 6:9]
+        elif 5 < down and right < 3:
+            block = puz[6:9, 0:3]
+        elif 5 < down and 2 < right < 6:
+            block = puz[6:9, 3:6]
+        elif 5 < down and 5 < right:
+            block = puz[6:9, 6:9]
+        else:
+            print("Index not in bounds so cant find block")
+
+        print('Column: \n', col, ' \nRow: \n', row, ' \nBlock: \n', block)
+        print('Original:', orig)
+
+        guess = 1
+        poss = [0]
+        howman = 0
+        while guess < 10:
+            if guess not in row and guess not in col and guess not in block and orig != guess:
+                poss.append(guess)
+                howman += 1
+            guess += 1
+        if howman > 0:
+            correct = 1
+            chooser = random.randint(howman) + 1
+            puz[down, right] = poss[chooser]
+        else:
+            correct = 0
+        if correct == 1:
+            if right < 8:
+                right += 1
+            else:
+                down += 1
+                right = 0
+        else:
+            puz[down, right] = 0
+            if right == 0:
+                right = 8
+                down -= 1
+            else:
+                right -= 1
+        print(puz)
+    else:
+        if correct == 1:
+            if right < 8:
+                right = right + 1
+            else:
+                down = down + 1
+                right = 0
+        else:
+            if right == 0:
+                right = 8
+                down = down - 1
+            else:
+                right = right - 1
+
+print(puz)
+t1 = time.time()
+process = t1 - t0
+print('Process Time: ', process)
---
+++ sudoku.py
@@ -0,0 +1,67 @@
+'''
+This program takes args from the command line
+assuming the args passed in are sudoku puzzles
+then solveing them
+'''
+import sys
+
+class Sudoku:
+    ''' wrapper class for a sudoku puzzle '''
+    def __init__(self, file_in):
+        self.puzzle = []
+        self.orig = []
+        # open the file with a temp fd
+        with open(file_in, 'r') as fin:
+            lines = fin.readlines()
+            # process all the lines
+            for line in lines:
+                if line != "\n":
+                    # no spaces and no nls
+                    self.puzzle.append(
+                        [c for c in line if c not in (' ', '\n')]
+                        )
+            # convert to nums
+            for row in self.puzzle:
+                for i, _ch in enumerate(row):
+                    row[i] = 0 if _ch == '-' else int(_ch)
+        self.orig = self.puzzle
+    def print(self):
+        ''' prints the current state of the puzzle '''
+        for row in self.puzzle:
+            print(row)
+    def solve(self):
+        ''' solves the puzzle stored in the class '''
+
+    def is_valid(self, row, col):
+        ''' is the passed index valid '''
+        return self.is_valid_row(row) and self.is_valid_col(col) and self.is_valid_blk(row, col)
+    def is_valid_row(self, row):
+        ''' checks a row and insures it is a valid row '''
+        # all the non zero vals in the given row
+        p_row = [i for i in self.puzzle[row] if i != 0]
+        # culls the dupes
+        return len(p_row) == len(set(p_row))
+    def is_valid_col(self, col):
+        ''' checks a col and insures it is a valid col '''
+        # all the non zero vals in the given column
+        p_col = [l[col] for l in self.puzzle if l[col] != 0]
+        # culls the dupes
+        return len(p_col) == len(set(p_col))
+    def is_valid_blk(self, row, col):
+        ''' checks a blk and insures it is a valid blk '''
+        # calculate the block index
+        r_blk = int(row / 3)
+        c_blk = int(col / 3)
+        # extrcat the block
+        block = [r[3 * c_blk: 3 * (c_blk + 1)]
+                 for r in self.puzzle[3 * r_blk:3 * (r_blk + 1)]]
+        # take out all the zeros
+        block = [val for sub in block for val in sub if val != 0]
+        return len(block) == len(set(block))
+
+# main exexution
+if __name__ == '__main__':
+    for a in sys.argv[1:]:
+        print(a)
+        sud = Sudoku(a)
+        sud.print()
